b6b59948556db9c7f21d5b271d65bb0a18392d02
Nico <plgueguy@gmail.com>
2020-08-12 00:19:30 -0400

    Created an address class that will store most addresses we use

---
+++ Dark Cloud Improved Version/Addresses.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dark_Cloud_Improved_Version
+{
+    class Addresses
+    {
+        public const int Gilda = 0x21CDD892;
+        public const int MagicCrystal = 0x202A35A0;
+        public const int Map = 0x202A359C;
+        public const int MiniMap = 0x202A35B0;
+        public const int Visibility = 0x202A359C;
+
+        public const int PositionX = 0x21D331D8;
+        public const int PositionY = 0x21D331D0;
+        public const int PositionZ = 0x21D331D4;
+        public const int DunPositionX = 0x21EA1D30;
+        public const int DunPositionY = 0x21EA1D38;
+        public const int DunPositionZ = 0x21EA1D34;
+    }
+}
