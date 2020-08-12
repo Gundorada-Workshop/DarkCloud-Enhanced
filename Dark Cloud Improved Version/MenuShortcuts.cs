c8f7c037315c765bfc81048128d59421305925cc
Nico <plgueguy@gmail.com>
2020-08-12 13:23:17 -0400

    Added MenuShortcuts class

---
+++ Dark Cloud Improved Version/MenuShortcuts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dark_Cloud_Improved_Version
+{
+    class MenuShortcut
+    {
+        public void AttributeShortcut()
+        {
+            if (Memory.ReadByte(Addresses.inputs1) == 10)
+            {
+                Console.WriteLine("DPad up");
+            }
+        }
+
+    }
+}
